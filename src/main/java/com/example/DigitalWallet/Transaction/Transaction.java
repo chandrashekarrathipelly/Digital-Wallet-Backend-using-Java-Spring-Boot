fd9c4c19dfb6b3cd2e2d7ce5bfa1f151d9a80546
chandrashekharrathipelly <chandrashekharrathipelly@gmail.com>
2024-08-12 13:37:31 +0530

    updated code

--- src/main/java/com/example/DigitalWallet/Transaction/Transaction.java
+++ src/main/java/com/example/DigitalWallet/Transaction/Transaction.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class Transaction {
 
